6dd6f556b19c309a3b86beeb64b65c6e4fd044a8
PC680 <hasanboronov0523@gmail.com>
2023-03-01 11:26:07 +0500

    asd

---
+++ app/src/main/java/uz/itschool/test/TestYonalishi.kt
@@ -0,0 +1,11 @@
+package uz.itschool.test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TestYonalishi : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_yonalishi)
+    }
+}
