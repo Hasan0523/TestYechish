fd425fcb366904738fb5d9594cf29a4bc1e6272a
PC680 <hasanboronov0523@gmail.com>
2023-03-01 11:26:07 +0500

    fvre

---
+++ app/src/main/java/uz/itschool/test/MathTest.kt
@@ -0,0 +1,11 @@
+package uz.itschool.test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MathTest : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_math_test)
+    }
+}
