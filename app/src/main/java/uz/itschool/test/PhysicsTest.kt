987fa9fef671d353e55fb7e02a0dee98f79376e9
PC902 <hasanboronov0523@gmail.com>
2023-03-07 14:51:10 +0500

    j

---
+++ app/src/main/java/uz/itschool/test/PhysicsTest.kt
@@ -0,0 +1,11 @@
+package uz.itschool.test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PhysicsTest : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_physics_test)
+    }
+}
