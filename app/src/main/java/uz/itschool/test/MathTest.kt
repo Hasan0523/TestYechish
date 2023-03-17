75860ba951a273fca44e2a39c94579e65c8cc8a3
PC902 <hasanboronov0523@gmail.com>
2023-03-17 09:35:53 +0500

    efger

--- app/src/main/java/uz/itschool/test/MathTest.kt
+++ app/src/main/java/uz/itschool/test/MathTest.kt
@@ -1,8 +1,11 @@
 package uz.itschool.test
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Layout
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.RadioGroup
 import android.widget.Toast
 
@@ -14,9 +17,12 @@ class MathTest : AppCompatActivity() {
     private lateinit var test3 : RadioGroup
     private lateinit var test4 : RadioGroup
     private lateinit var test5 : RadioGroup
+    var Yakun = mutableListOf<RadioGroup>()
 
 
 
+
+    @SuppressLint("MissingInflatedId", "WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_math_test)
@@ -26,7 +32,13 @@ class MathTest : AppCompatActivity() {
         test4 = findViewById(R.id.test4)
         test5 = findViewById(R.id.test5)
         tugatish = findViewById(R.id.tugat)
+        Yakun.add(test1)
+        Yakun.add(test2)
+        Yakun.add(test3)
+        Yakun.add(test4)
+        Yakun.add(test5)
         tugatish.setOnClickListener {
+
             var ball = 0
             if (test1.checkedRadioButtonId == R.id.savol1){
                 ball+=1
@@ -44,6 +56,7 @@ class MathTest : AppCompatActivity() {
                 ball+=1
             }
             Toast.makeText(applicationContext, ball.toString(), Toast.LENGTH_SHORT).show()
+
         }
     }
 }
